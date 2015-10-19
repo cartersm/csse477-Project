97e7079bf21fb96ce6277da28648e301c49c7caf
cartersm <cartersm@rose-hulman.edu>
2015-10-18 20:35:31 -0400

    Hi, I'm stupid.
    added "response = "  back in so we don't have a null response on GET. Or
    everything, for that matter.

--- edu.rosehulman.sws/src/protocol/request_handler/DeleteRequestHandler.java
+++ edu.rosehulman.sws/src/protocol/request_handler/DeleteRequestHandler.java
@@ -1,15 +1,47 @@
 package protocol.request_handler;
 
+import java.io.File;
+
 import protocol.HttpRequest;
 import protocol.HttpResponse;
+import protocol.Protocol;
 import server.Server;
 
 public class DeleteRequestHandler implements RequestHandler {
 
 	@Override
 	public HttpResponse handle(HttpRequest request, Server server) {
-		// TODO Auto-generated method stub
-		return null;
+		HttpResponse response = null;
+		// Handling POST request here
+		// Get relative URI path from request
+		String uri = request.getUri();
+		// Get root directory path from server
+		String rootDirectory = server.getRootDirectory();
+		// Combine them together to form absolute file path
+		File file = new File(rootDirectory + uri);
+
+		// Check if the file exists
+		if (file.exists()) {
+			if (file.isDirectory()) {
+				// Look for default index.html file in a directory
+				String location = rootDirectory + uri + System.getProperty("file.separator") + Protocol.DEFAULT_FILE;
+				file = new File(location);
+				if (file.exists()) {
+					// TODO: delete file
+					// response = HttpResponseFactory.create200OK(file, Protocol.CLOSE);
+				} else {
+					// TODO: 400 or 404?
+					// response = HttpResponseFactory.create200OK(Protocol.CLOSE);
+				}
+			} else {
+				// TODO: delete file
+				// response = HttpResponseFactory.create200OK(file, Protocol.CLOSE);
+			}
+		} else {
+			// TODO: 400 or 404?
+			// response = HttpResponseFactory.create200OK(Protocol.CLOSE);
+		}
+		return response;
 	}
 
 }
--- edu.rosehulman.sws/src/protocol/request_handler/PostRequestHandler.java
+++ edu.rosehulman.sws/src/protocol/request_handler/PostRequestHandler.java
@@ -1,15 +1,50 @@
 package protocol.request_handler;
 
+import java.io.File;
+
 import protocol.HttpRequest;
 import protocol.HttpResponse;
+import protocol.HttpResponseFactory;
+import protocol.Protocol;
 import server.Server;
 
 public class PostRequestHandler implements RequestHandler {
 
 	@Override
 	public HttpResponse handle(HttpRequest request, Server server) {
-		// TODO Auto-generated method stub
-		return null;
+		HttpResponse response = null;
+		// Handling POST request here
+		// Get relative URI path from request
+		String uri = request.getUri();
+		// Get root directory path from server
+		String rootDirectory = server.getRootDirectory();
+		// Combine them together to form absolute file path
+		File file = new File(rootDirectory + uri);
+
+		String body = new String(request.getBody());
+		
+		// Check if the file exists
+		if (file.exists()) {
+			if (file.isDirectory()) {
+				// Look for default index.html file in a directory
+				String location = rootDirectory + uri + System.getProperty("file.separator") + Protocol.DEFAULT_FILE;
+				file = new File(location);
+				if (file.exists()) {
+					// TODO: append body to file
+					// response = HttpResponseFactory.create200OK(file, Protocol.CLOSE);
+				} else {
+					// TODO: create file and write body
+					// response = HttpResponseFactory.create200OK(Protocol.CLOSE);
+				}
+			} else {
+				// TODO: append body to file
+				// response = HttpResponseFactory.create200OK(file, Protocol.CLOSE);
+			}
+		} else {
+			// TODO: create file and write body
+			// response = HttpResponseFactory.create200OK(Protocol.CLOSE);
+		}
+		return response;
 	}
 
 }
